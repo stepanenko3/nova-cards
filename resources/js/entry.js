ce92c17273aa8a03bddaf5e3e31db6989bdc93b3
Artem Stepanenko <stpnnk.artem@gmail.com>
2022-05-10 14:06:38 +0300

    updates

--- resources/js/entry.js
+++ resources/js/entry.js
@@ -1,17 +1,33 @@
+import LoadingCardWithButton from './components/LoadingCardWithButton'
+//
+import SystemResources from './cards/SystemResources'
+import Versions from './cards/Versions'
+import Html from './cards/Html'
+import ScheduledJobs from './cards/ScheduledJobs'
+import Percentage from './cards/Percentage'
+import Linkable from './cards/Linkable'
+import Ssl from './cards/Ssl'
+import Countdown from './cards/Countdown'
+import WorldClock from './cards/WorldClock'
+import BlockchainExchange from './cards/BlockchainExchange'
+import Environment from './cards/Environment'
+import Embed from './cards/Embed'
+import Cache from './cards/Cache'
+
 Nova.booting((Vue) => {
-    Vue.component('LoadingCardWithButton', require('./components/LoadingCardWithButton').default);
+    Vue.component('LoadingCardWithButton', LoadingCardWithButton);
     //
-    Vue.component('system-resources-card', require('./components/SystemResources').default);
-    Vue.component('versions-card', require('./components/Versions').default);
-    Vue.component('html-card', require('./components/Html').default);
-    Vue.component('scheduled-jobs-card', require('./components/ScheduledJobs').default);
-    Vue.component('percentage-card', require('./components/Percentage').default);
-    Vue.component('linkable-card', require('./components/Linkable').default);
-    Vue.component('ssl-card', require('./components/Ssl').default);
-    Vue.component('countdown-card', require('./components/Countdown').default);
-    Vue.component('world-clock-card', require('./components/WorldClock').default);
-    Vue.component('blockchain-exchange-card', require('./components/BlockchainExchange').default);
-    Vue.component('environment-card', require('./components/Environment').default);
-    Vue.component('embed-card', require('./components/Embed').default);
-    Vue.component('cache-card', require('./components/Cache').default);
-  });
+    Vue.component('system-resources-card', SystemResources);
+    Vue.component('versions-card', Versions);
+    Vue.component('html-card', Html);
+    Vue.component('scheduled-jobs-card', ScheduledJobs);
+    Vue.component('percentage-card', Percentage);
+    Vue.component('linkable-card', Linkable);
+    Vue.component('ssl-card', Ssl);
+    Vue.component('countdown-card', Countdown);
+    Vue.component('world-clock-card', WorldClock);
+    Vue.component('blockchain-exchange-card', BlockchainExchange);
+    Vue.component('environment-card', Environment);
+    Vue.component('embed-card', Embed);
+    Vue.component('cache-card', Cache);
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,9 @@
-let mix = require('laravel-mix');
-let path = require('path');
+let mix = require('laravel-mix')
 
+require('./nova.mix')
 
-console.log(path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'));
 mix
   .setPublicPath('dist')
   .js('resources/js/entry.js', 'js')
   .vue({ version: 3 })
-  .webpackConfig({
-    externals: {
-      vue: 'Vue',
-    },
-    output: {
-      uniqueName: 'stepanenko3/nova-cards',
-    },
-  })
-  .alias({
-    'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
-  });
+  .nova('stepanenko3/nova-cards')
